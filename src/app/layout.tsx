fb122c812e81c9b3fb80faeb47f502fb8f1c4c04
54lv4d0r <55425453+54lv4d0r@users.noreply.github.com>
2026-07-21 22:31:43 -0600

    Update metadata title and description in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,15 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Mis XV - Natasha | Invitación Digital",
-  description: "Te invito a compartir conmigo este día tan especial.",
+  title: "Mis XV - Natasha",
+  description: "Invitación Digital",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="es">
       <body>{children}</body>
