6f12b2bd98bb2af23c3a5ed727d6006d9e87fc24
54lv4d0r <55425453+54lv4d0r@users.noreply.github.com>
2026-07-23 23:46:52 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,14 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Mis XV - Marelynk Nicole Altamirano",
   description: "Invitación Digital",
+  openGraph: {
+    title: "Mis XV - Marelynk Nicole Altamirano",
+    description: "Invitación Digital",
+    url: "https://koneko-invitacion.vercel.app",
+    siteName: "Invitación Digital",
+    locale: "es_ES",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
